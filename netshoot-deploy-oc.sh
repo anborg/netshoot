7a8b25772055f1091ec89f4c8f47cab15742a86e
test <test@gmail.com>
2021-08-14 21:27:49 -0400

    init

---
+++ netshoot-deploy-oc.sh
@@ -0,0 +1,8 @@
+#oc delete -f manifest/oshift/pod.yml
+#oc create -f manifest/oshift/pod.yml
+
+#oc delete -f manifest/oshift/deploy-rhel.yml
+#oc create -f manifest/oshift/deploy-rhel.yml
+
+oc delete -f manifest/oshift/deploy-netshoot.yml
+oc create -f manifest/oshift/deploy-netshoot.yml
